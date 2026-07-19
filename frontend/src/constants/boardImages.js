1a420dc3293b824560c9b32c6ae3177860ff6f27
İsmet Baltacıoğlu <ismetbaltacioglu@Ismet-Air.lan>
2026-07-19 21:03:31 +0300

    kutular güncellendi

--- frontend/src/constants/boardImages.js
+++ frontend/src/constants/boardImages.js
@@ -11,50 +11,50 @@ export const BOARD_IMAGES = {
   // ALT KENAR (0 - 13)
   0: 'https://i.pinimg.com/736x/25/83/25/258325e218093f78599368d65312663d.jpg', // Başlangıç
   1: 'https://kalkinmagalerisi.org.tr/_next/image?url=https%3A%2F%2Fkalkinmagalerisi.org.tr%2F%2Ffiles%2Fupload%2Fimages%2Fprocessed%2Fo_1gf2s7jim426g6f1vhojfh52t7.jpg&w=3840&q=75', // Yozgat
-  2: '', // Şans Kartı
-  3: '', // Kars
-  4: '', // Hazine Vergisi
-  5: '', // Uluslararası Liman
-  6: '', // Edirne
-  7: '', // Şans Kartı
-  8: '', // Tekirdağ
-  9: '', // Çanakkale
-  10: '', // Samsun
-  11: '', // Trabzon
-  12: '', // Rize
-  13: '', // Hapis / Denetim
+  2: 'https://creativegroup-blog-prd-eu-west-1-recharge-blog.s3.eu-west-1.amazonaws.com/blog/wp-content/uploads/2023/04/17125540/Does_an_anonymous_SIM_card_exist_RE.jpg', // Şans Kartı
+  3: 'https://www.serka.gov.tr/assets/user_files/karsaniantikkenti.jpg', // Kars
+  4: 'https://ziraatodasi.gen.tr/wp-content/uploads/2021/01/vergi-2.jpg', // Hazine Vergisi
+  5: 'https://c4.wallpaperflare.com/wallpaper/691/692/932/the-city-work-the-situation-ships-wallpaper-preview.jpg', // Uluslararası Liman
+  6: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRHesog6U780opbVRwqcuZRC8ieZ4SfXluz3RShINT0rMiPvtGaml0D0I&s=10', // Edirne
+  7: 'https://creativegroup-blog-prd-eu-west-1-recharge-blog.s3.eu-west-1.amazonaws.com/blog/wp-content/uploads/2023/04/17125540/Does_an_anonymous_SIM_card_exist_RE.jpg', // Şans Kartı
+  8: 'https://www.tekirdag.bel.tr/content/WebSource/image/slide/1.jpg', // Tekirdağ
+  9: 'https://surmenosgb.com.tr/wp-content/uploads/2024/03/canakkale-zaferi.jpg', // Çanakkale
+  10: 'https://www.samsunharitasi.com/Icerik/Large/www.samsunharitasi.com_2_samsun-tarihi_WK9G11LZDLHC.png', // Samsun
+  11: 'https://images.unsplash.com/photo-1673521768190-7847013f1b9d?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHRyYWJ6b258ZW58MHx8MHx8fDA%3D', // Trabzon
+  12: 'https://blog.otelegidelim.com/wp-content/uploads/2024/08/rize-1200x600-1.jpg', // Rize
+  13: 'https://media.istockphoto.com/id/2114506677/tr/fotoğraf/inside-prison-corridor-at-night-cells-with-lights-on-and-wet-floor.jpg?s=612x612&w=0&k=20&c=hBU9KNsAsetfloTqlNzBE17kx1zNqXzawQlArS6NMi4=', // Hapis / Denetim
 
   // SOL KENAR (14 - 19)
-  14: '', // Şanlıurfa
-  15: '', // Hammadde Üretim Tesisi
-  16: '', // Diyarbakır
-  17: '', // Şans Kartı
-  18: '', // Gaziantep
-  19: '', // Borsa / Halka Arz
+  14: 'https://c1.wallpaperflare.com/preview/267/603/706/sanliurfa-urfa-balikligöl-carp.jpg', // Şanlıurfa
+  15: 'https://www.epozem.com/resimler/epoksi/hammadde.jpeg-2.jpg', // Hammadde Üretim Tesisi
+  16: 'https://cdn.renticar.com/cms/eba922c4_c967_4e9e_a89e_b355f30f3e0f_eef9e76c2f.jpeg', // Diyarbakır
+  17: 'https://creativegroup-blog-prd-eu-west-1-recharge-blog.s3.eu-west-1.amazonaws.com/blog/wp-content/uploads/2023/04/17125540/Does_an_anonymous_SIM_card_exist_RE.jpg', // Şans Kartı
+  18: 'https://www.kulturportali.gov.tr/contents/images/20170301092804156_kale_genel15x10cm.jpg', // Gaziantep
+  19: 'https://www.entererp.com/_next/image?url=https%3A%2F%2Fmain.entererp.com%2Fapi%2Fstore%2Fcommon%2Fimage%2F64b2f891170baad3887ed870&w=3840&q=90', // Borsa / Halka Arz
 
   // ÜST KENAR (20 - 32)
-  20: '', // Merkez Bankası
-  21: '', // Eskişehir
-  22: '', // Konya
-  23: '', // Kayseri
-  24: '', // Sanayi Fabrikası
-  25: '', // Muğla
-  26: '', // Mersin
-  27: '', // Bursa
-  28: '', // Şans Kartı
-  29: '', // Medya Şirketi
-  30: '', // Antalya
-  31: '', // İzmir
-  32: '', // Aydın
+  20: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTLi3o9qdifh0YQiP22KU0rS0V72uRtuMzb0_-m0RAi7dty_rXyLIzhcfR&s=10', // Merkez Bankası
+  21: 'https://upload.wikimedia.org/wikipedia/commons/3/31/Adalar_porsuk1.JPG', // Eskişehir
+  22: 'https://www.versiyonturizm.com/upload/galeri/555_konya-gezilecek-yerler.jpg', // Konya
+  23: 'https://cdnp.flypgs.com/files/Sehirler-long-tail/Kayseri/kayseri-heykel-cami-kule.jpg', // Kayseri
+  24: 'https://kortan.tr/wp-content/uploads/2024/04/fabrika-satisi.jpg', // Sanayi Fabrikası
+  25: 'https://www.guvenvillas.com.tr/assets/images/blog/big/mugla-gezi_1.jpg', // Muğla
+  26: 'https://www.etstur.com/letsgo/wp-content/uploads/2026/07/01-mersin-letsgo.jpg', // Mersin
+  27: 'https://www.tatilcity.net/uploads/0000/1/2024/10/04/bursa.jpg', // Bursa
+  28: 'https://creativegroup-blog-prd-eu-west-1-recharge-blog.s3.eu-west-1.amazonaws.com/blog/wp-content/uploads/2023/04/17125540/Does_an_anonymous_SIM_card_exist_RE.jpg', // Şans Kartı
+  29: 'https://cdn.britannica.com/25/93825-050-D1300547/collection-newspapers.jpg', // Medya Şirketi
+  30: 'https://t3.ftcdn.net/jpg/05/71/76/86/360_F_571768620_I0OKBXP3S3KoAAOCZ59GrBjiG8SysCfY.jpg', // Antalya
+  31: 'https://images.unsplash.com/photo-1701428588034-5893b2512a68?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aXptaXIlMjB0dXJrZXl8ZW58MHx8MHx8fDA%3D', // İzmir
+  32: 'https://thekalucci.com/cdn/shop/files/preview_images/A_KSD_NORMAL_4K_3840_2160_H.265_0649.jpg?v=1697896738', // Aydın
 
   // SAĞ KENAR (33 - 39)
-  33: '', // Yeraltı Kumarhanesi
-  34: '', // Şans Kartı
-  35: '', // AVM
-  36: '', // Servet Vergisi
-  37: '', // Türkiye Varlık Fonu
-  38: '', // Ankara
-  39: '', // İstanbul
+  33: 'https://static.wikia.nocookie.net/kurtlar-vadisi/images/e/e8/CakirKumarhane.png/revision/latest/scale-to-width-down/1200?cb=20250411001350&path-prefix=tr', // Yeraltı Kumarhanesi
+  34: 'https://creativegroup-blog-prd-eu-west-1-recharge-blog.s3.eu-west-1.amazonaws.com/blog/wp-content/uploads/2023/04/17125540/Does_an_anonymous_SIM_card_exist_RE.jpg', // Şans Kartı
+  35: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp3gwgo4OGw911RSG-kxG3xZjDlaSbIbqOB44nKa5tMNPIdzyoTAc-B_w&s=10', // AVM
+  36: 'https://ziraatodasi.gen.tr/wp-content/uploads/2021/01/vergi-2.jpg', // Servet Vergisi
+  37: 'https://web-cdnprod.aa.com.tr/uploads/Contents/2022/07/09/thumbs_b_c_141af7e34672be4b5f727354d22d6ce5.jpg?v=111716', // Türkiye Varlık Fonu
+  38: 'https://media.istockphoto.com/id/471509753/tr/fotoğraf/anitkabir-at-dusk.jpg?s=612x612&w=0&k=20&c=CH4mMyH_Qvvnx5z4abtQpHllmP0zzfz8kToDmFxT_XM=', // Ankara
+  39: 'https://i.pinimg.com/736x/d9/5e/68/d95e6832254a4bd5974ad2a45d9a6932.jpg', // İstanbul
 };
 
 // Eğer yukarıda bir kareye resim yazmazsanız (boş bırakırsanız) bu varsayılan sistem çalışır:
